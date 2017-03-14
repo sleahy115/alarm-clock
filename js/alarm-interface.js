2f112c4069d4005e264a57fe9c7f3b7adaba2020
Stella Huayhuaca and Sarah Leahy <stella@>
2017-03-14 11:53:51 -0700

    add alarm function

--- js/alarm-interface.js
+++ js/alarm-interface.js
@@ -1,3 +1,22 @@
+// var Alarm = require('./../js/alarm.js').alarmModule;
+
 $(document).ready(function(){
-  $('#time').text(moment());
+  var currentTime= moment().format('LT');
+  // var currentDateTime= moment();
+  console.log(currentTime);
+  $('#time').text(currentTime);
+
+  $('#alarmSet').submit(function (event) {
+    event.preventDefault();
+    var alarmTime = $('#timeSet').val()+ " AM";
+    console.log(alarmTime);
+    if(alarmTime === currentTime) {
+      alert("this is the alarm going off!");
+    }
+    // var alarmDate = $('#dateSet').val();
+    // var alarmSet = alarmDate + alarmTime;
+    // console.log(alarmSet);
+    // var alarm = moment(alarmSet).add(1, 'hours');
+    // console.log(alarm);
+  });
 });
---
+++ js/alarm.js
@@ -0,0 +1,12 @@
+var options="";
+
+for (var i=1; i<=12; i++){
+  options += "<option>"+ i + "</option>";
+}
+
+document.getElementById('hours').innerHTML= options;
+
+
+
+
+// exports.alarmModule = Alarm;
